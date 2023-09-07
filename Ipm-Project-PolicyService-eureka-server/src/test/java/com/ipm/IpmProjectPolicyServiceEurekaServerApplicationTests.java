0ed725c9b0a22a9b6ef7d215de0a74c347e6ca9d
THEERTHA T <74696229+THEERTHA-T@users.noreply.github.com>
2023-09-07 19:28:16 +0530

    Eureka Server

---
+++ Ipm-Project-PolicyService-eureka-server/src/test/java/com/ipm/IpmProjectPolicyServiceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpmProjectPolicyServiceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
